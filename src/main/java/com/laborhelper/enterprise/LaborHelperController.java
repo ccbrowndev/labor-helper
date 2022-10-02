a8d56e5f093143de4c04ca22b5866efa6a216324
ccbrowndev <46230726+ccbrowndev@users.noreply.github.com>
2022-10-02 17:07:58 -0400

    Add registration endpoint and add bootstrap CSS to registration.html

--- src/main/java/com/laborhelper/enterprise/LaborHelperController.java
+++ src/main/java/com/laborhelper/enterprise/LaborHelperController.java
@@ -2,18 +2,23 @@ package com.laborhelper.enterprise;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LaborHelperController {
 
     /**
-     * Handle the root (/) endpoint and return a start page
-     * @return
+     * Handle the root (/) endpoint
+     * @return the home page of the application
      */
     @GetMapping ("/")
     public String index() {
         return "start";
     }
 
+    /**
+     * Handle the registration (/registration) endpoint
+     * @return a login and registration area
+     */
+    @GetMapping ("/registration")
+    public String registration() { return "registration";}
 }
