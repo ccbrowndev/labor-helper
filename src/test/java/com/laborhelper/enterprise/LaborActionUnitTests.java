ecb7ac11cb952b5938bc9f7c96cb0a5d1eb4e81c
ccbrowndev <46230726+ccbrowndev@users.noreply.github.com>
2022-10-13 21:43:49 -0400

    Implement some hard coded data to the DAO stub so test passes

--- src/test/java/com/laborhelper/enterprise/LaborActionUnitTests.java
+++ src/test/java/com/laborhelper/enterprise/LaborActionUnitTests.java
@@ -1,5 +1,7 @@
 package com.laborhelper.enterprise;
 
+import com.laborhelper.enterprise.dao.ILaborActionDAO;
+import com.laborhelper.enterprise.dao.LaborActionDAOStub;
 import com.laborhelper.enterprise.dto.LaborAction;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +11,7 @@ import static org.springframework.test.util.AssertionErrors.assertEquals;
 @SpringBootTest
 class LaborActionUnitTests {
 
-    private ILaborActionService laborActionService;
+    private ILaborActionDAO laborActionService = new LaborActionDAOStub();
     private LaborAction laborAction;
 
     @Test
