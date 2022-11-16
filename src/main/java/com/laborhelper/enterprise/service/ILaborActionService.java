6bc32e0831a2ea8140867822ae40c67d353f6cc0
daileytj2 <54542601+daileytj2@users.noreply.github.com>
2022-11-16 15:55:47 -0500

    Making the ILaborActionService interface

---
+++ src/main/java/com/laborhelper/enterprise/service/ILaborActionService.java
@@ -0,0 +1,17 @@
+package com.laborhelper.enterprise.service;
+
+import com.laborhelper.enterprise.dto.LaborAction;
+
+import java.util.List;
+
+public interface ILaborActionService {
+
+    LaborAction fetchByID(int id);
+
+    void delete(int id) throws Exception;
+
+    LaborAction save(LaborAction laborAction) throws Exception;
+
+    List<LaborAction> fetchAll();
+
+}
